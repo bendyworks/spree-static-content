767faabda385518931dff9c885780ba45aad8715
Jaymes Waters and Nick Karpenske <dev+jaym3s+randland@bendyworks.com>
2010-02-17 14:50:34 -0600

    reworked views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 map.namespace :admin do |admin|
   admin.resources :pages do |page|
-    page.resources :taxons, as: => 'pages_taxons', :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
+    page.resources :taxons, :as => 'pages_taxons', :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
   end
 end 
 map.static '/static/*path', :controller => 'static_content', :action => 'show'
